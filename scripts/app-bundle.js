d00b5258f1615532a6bb957221ef8487f82960cc
david-steinberg-codes <david@steinberg.codes>
2017-10-12 23:31:07 -0400

    Built application properly

--- scripts/app-bundle.js
+++ scripts/app-bundle.js
@@ -14,7 +14,7 @@ define('app',['exports'], function (exports) {
   var App = exports.App = function App() {
     _classCallCheck(this, App);
 
-    this.message = 'Hello World!';
+    this.message = 'Hello World! (from Aurelia)';
   };
 });
 define('environment',["exports"], function (exports) {
